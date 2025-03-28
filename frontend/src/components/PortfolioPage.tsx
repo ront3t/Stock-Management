6185022d9116eaa678aba7cd3d5c9c619d9f72b5
ront3t <ront3t@gmail.com>
2025-03-28 17:35:44 +0300

    login page

---
+++ frontend/src/components/PortfolioPage.tsx
@@ -0,0 +1,6 @@
+export const PortfolioPage = () =>{
+    return (<div>
+        PortfolioPage
+    </div>)
+}
+
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
+  baseURL: 'http://localhost:3000',
 });
 
 API.interceptors.request.use(config => {
